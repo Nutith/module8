8cff88d13c32f0a513ce4c356010f9f69b2a0f0d
Nutith <dvedenev@hotmail.com>
2024-11-01 12:49:23 +0300

    Задание "Программистам всё можно"

---
+++ 1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        result = a + b
+    except TypeError:
+        result = f'{a}{b}'
+
+    return result
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
